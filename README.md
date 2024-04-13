5f9890e35d04c37814cecfc93e183988600c283c
colmpat <cplang@dons.usfca.edu>
2024-04-13 11:22:47 -0700

    add v0

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # 1brc
+
+## Devlog
+
+#### v0
+
+Setup the project and got something basic working. Added profiling and a timer mechanism to measure our
+approach from here on out. To profile, I'm using golang's built-in profiling tool called `pprof`. This
+dumps a profile file called `cpu.prof` which you can visualize with `pprof -http=localhost:8080 cpu.prof`.
+This initial version is meant to serve as a baseline for future optimizations!
+
+**Runtime**: 3m41.311502687s
--- main.go
+++ main.go
@@ -1,7 +1,103 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"runtime/pprof"
+	"slices"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Fprintf(os.Stderr, "\n%s took %v\n", name, time.Since(start))
+	}
+}
+
+type Results map[string]Result
+
+type Result struct {
+	Min   float64
+	Max   float64
+	Sum   float64
+	Count int
+}
+
+func parseFloat(s string) float64 {
+	f, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		panic(err)
+	}
+	return f
+}
+
+func printResults(results Results) {
+	keys := make([]string, 0, len(results))
+	for k := range results {
+		keys = append(keys, k)
+	}
+	slices.Sort(keys)
+
+	fmt.Print("{")
+	for _, k := range keys[:len(keys)-1] {
+		r := results[k]
+		avg := r.Sum / float64(r.Count)
+		fmt.Printf("%s=%.1f/%.1f/%.1f, ", k, r.Min, avg, r.Max)
+	}
+	k := keys[len(keys)-1]
+	r := results[k]
+	avg := r.Sum / float64(r.Count)
+	fmt.Printf("%s=%.1f/%.1f/%.1f}", k, r.Min, avg, r.Max)
+}
 
 func main() {
-	fmt.Println("vim-go")
+	pf, err := os.Create("cpu.prof")
+	if err != nil {
+		log.Fatal("could not create CPU profile: ", err)
+	}
+	defer pf.Close()
+	if err := pprof.StartCPUProfile(pf); err != nil {
+		log.Fatal("could not start CPU profile: ", err)
+	}
+	defer pprof.StopCPUProfile()
+
+	defer timer("main")()
+	filePath := "data/measurements.txt"
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	results := make(Results)
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		station, tempStr, _ := strings.Cut(line, ";")
+		temp := parseFloat(tempStr)
+
+		if r, ok := results[station]; !ok {
+			results[station] = Result{
+				Min:   temp,
+				Max:   temp,
+				Sum:   temp,
+				Count: 1,
+			}
+		} else {
+			r.Min = math.Min(r.Min, temp)
+			r.Max = math.Max(r.Max, temp)
+			r.Sum += temp
+			r.Count++
+		}
+	}
+
+	printResults(results)
 }
